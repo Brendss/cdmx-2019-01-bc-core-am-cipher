53e9f37e3500a3d64be6e6abccd234022e04452d
Brendss <brenda.ale_@hotmail.com>
2019-01-14 18:16:42 -0600

    Archivo con botones de regresar y seleccionar opcion de la primer pantalla funcionando. También funciona el ocultar pantallas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.getElementById('Crear').addEventListener('click',page1);
 document.getElementById('Descifrar').addEventListener('click',page2);
-
+document.getElementById('regresar').addEventListener('click',page3);
+document.getElementById('regresar2').addEventListener('click',page3);
 
 
 
@@ -15,4 +16,10 @@ function page1() {
     document.getElementById('Pantalla1').style.display='none';
     document.getElementById('Pantalla3').style.display='block';
     document.getElementById('Pantalla2').style.display='none';
+  }
+
+  function page3() {
+    document.getElementById('Pantalla1').style.display='block';
+    document.getElementById('Pantalla2').style.display='none';
+    document.getElementById('Pantalla3').style.display='none';
   }
