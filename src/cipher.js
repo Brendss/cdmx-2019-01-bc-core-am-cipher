5a34cc7f65370de23b4b7fc66a82cce7cf815e01
Brendss <brenda.ale_@hotmail.com>
2019-01-16 11:13:35 -0600

    Proyecto que ya guarda el texto de la segunda pantalla y la muestra en consola; también ya convierte texto en mayusculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,11 @@
+
 window.cipher = {
-  // ... 
+  encode: (string) => {
+    console.log(string)
+   // for (let i=0; i < mayus.length; i++) {
+
+    //}
+decode: () =>{}
+  }
+
 };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
-document.getElementById('Crear').addEventListener('click',page1);
-document.getElementById('Descifrar').addEventListener('click',page2);
-document.getElementById('regresar').addEventListener('click',page3);
-document.getElementById('regresar2').addEventListener('click',page3);
+let pagina1= document.getElementById('Crear').addEventListener('click', page1);
+let paina2= document.getElementById('Descifrar').addEventListener('click', page2);
+let pagina3= document.getElementById('regresar').addEventListener('click', page3);
+let pagina4= document.getElementById('regresar2').addEventListener('click', page3);
+let crear2 = document.getElementById('Crear2');
+
 
 
 
 
 function page1() {
-    document.getElementById('Pantalla1').style.display='none';
-    document.getElementById('Pantalla2').style.display='block';
-    document.getElementById('Pantalla3').style.display='none';
-  }
+  document.getElementById('Pantalla1').style.display = 'none';
+  document.getElementById('Pantalla2').style.display = 'block';
+  document.getElementById('Pantalla3').style.display = 'none';
+}
 
-  function page2() {
-    document.getElementById('Pantalla1').style.display='none';
-    document.getElementById('Pantalla3').style.display='block';
-    document.getElementById('Pantalla2').style.display='none';
-  }
+function page2() {
+  document.getElementById('Pantalla1').style.display = 'none';
+  document.getElementById('Pantalla3').style.display = 'block';
+  document.getElementById('Pantalla2').style.display = 'none';
+}
 
-  function page3() {
-    document.getElementById('Pantalla1').style.display='block';
-    document.getElementById('Pantalla2').style.display='none';
-    document.getElementById('Pantalla3').style.display='none';
-  }
+function page3() {
+  document.getElementById('Pantalla1').style.display = 'block';
+  document.getElementById('Pantalla2').style.display = 'none';
+  document.getElementById('Pantalla3').style.display = 'none';
+}
+
+const offset = document.getElementById('desplazar');
+let mayus = document.getElementById('textACod');
+
+crear2.addEventListener('click', () => {
+  mayus = mayus.value.toUpperCase();
+  window.cipher.encode(mayus)
+})
+
+
