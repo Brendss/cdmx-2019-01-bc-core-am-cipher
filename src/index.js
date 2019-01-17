e0fac43b2380620b7cbadc25df753d5363edec02
Brendss <brenda.ale_@hotmail.com>
2019-01-16 21:19:03 -0600

    Archivos corregidos y con las funciones correctas para codificar; la primer y segunda pantalla funcionan perfectamente. Ya pasa los test de codificar

--- src/index.js
+++ src/index.js
@@ -1,36 +1,40 @@
-document.getElementById('Crear').addEventListener('click', page1);
-document.getElementById('Descifrar').addEventListener('click', page2);
-document.getElementById('regresar').addEventListener('click', page3);
-document.getElementById('regresar2').addEventListener('click', page3);
-let crear2 = document.getElementById('Crear2');
+document.getElementById("Crear").addEventListener("click", page1);
+document.getElementById("Descifrar").addEventListener("click", page2);
+document.getElementById("regresar").addEventListener("click", page3);
+document.getElementById("regresar2").addEventListener("click", page3);
+
 
 
 function page1() {
-  document.getElementById('Pantalla1').style.display = 'none';
-  document.getElementById('Pantalla2').style.display = 'block';
-  document.getElementById('Pantalla3').style.display = 'none';
+  document.getElementById("Pantalla1").style.display = "none";
+  document.getElementById("Pantalla2").style.display = "block";
+  document.getElementById("Pantalla3").style.display = "none";
 }
 
 function page2() {
-  document.getElementById('Pantalla1').style.display = 'none';
-  document.getElementById('Pantalla3').style.display = 'block';
-  document.getElementById('Pantalla2').style.display = 'none';
+  document.getElementById("Pantalla1").style.display = "none";
+  document.getElementById("Pantalla3").style.display = "block";
+  document.getElementById("Pantalla2").style.display = "none";
 }
 
 function page3() {
-  document.getElementById('Pantalla1').style.display = 'block';
-  document.getElementById('Pantalla2').style.display = 'none';
-  document.getElementById('Pantalla3').style.display = 'none';
+  document.getElementById("Pantalla1").style.display = "block";
+  document.getElementById("Pantalla2").style.display = "none";
+  document.getElementById("Pantalla3").style.display = "none";
 }
 
-let offset = document.getElementById('desplazar');
-let mayus = document.getElementById('textACod');
+const desplazar = document.getElementById("desplazar");
+const textACod = document.getElementById("textACod");
+const crear2 = document.getElementById("Crear2");
 
-crear2.addEventListener('click', () => {
-  mayus = mayus.value.toUpperCase();
-  offset = offset.value;
-  document.getElementById('textCod').innerHTML=window.cipher.encode(offset,mayus);
+crear2.addEventListener("click", () => {
+  const mayus = textACod.value.toUpperCase();
+  const offset = desplazar.value;
+  document.getElementById("textCod").innerHTML=window.cipher.encode(offset,mayus);
 });
+
+
+
 
 
   
