064f1f876750d46a120254d603e99fde4beb7356
Brendss <brenda.ale_@hotmail.com>
2019-01-23 14:08:04 -0600

    proyecto completo en JS,y con CSS completo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ document.getElementById("Descifrar").addEventListener("click", page2);
 document.getElementById("regresar").addEventListener("click", page3);
 document.getElementById("regresar2").addEventListener("click", page3);
 
+//para limpiar campos
+document.getElementById("borrar").addEventListener("click",clear);
+document.getElementById("borrar2").addEventListener("click",clear2);
 
 //cambio de pantallas
 function page1() {
@@ -23,6 +26,20 @@ function page3() {
   document.getElementById("Pantalla2").style.display = "none";
   document.getElementById("Pantalla3").style.display = "none";
 }
+
+//para limpiar campos
+function clear(){
+  document.getElementById("desplazar").value="";
+  document.getElementById("textACod").value="";
+  document.getElementById("textCod").innerHTML="";
+}
+
+function clear2(){
+  document.getElementById("desplazar2").value="";
+  document.getElementById("textADecod").value="";
+  document.getElementById("textDecod").innerHTML="";
+}
+
 
 //para codificar
 const desplazar = document.getElementById("desplazar");
